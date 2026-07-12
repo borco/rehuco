a3b3a8370d1954cf89c8eaa08445ae56e9311f6d
Ioan Călin <iborco@gmail.com>
2026-07-12 15:50:10 +0300

    refs #52: feat: rewrite description image references on .tc conversion
    Maps a description's embedded ![]() references -- bare, extension-less,
    path-prefixed, or naming either a slot's winning or losing filename -- to
    the new slugNN name a real conversion would give that screenshot.

--- packages/rehuco-core/src/rehuco_core/__init__.py
+++ packages/rehuco-core/src/rehuco_core/__init__.py
@@ -1,6 +1,7 @@
 """rehuco-core: shared library for models, .rehu I/O, and sync primitives."""
 
 from rehuco_core.rehu_document import RehuDocument, RehuFormatError
+from rehuco_core.tc_description import TcDescriptionRewriter, rewrite_description_images
 from rehuco_core.tc_document import TcDocument, load_tc, tc_to_rehu_data
 from rehuco_core.tc_screenshots import ScreenshotRename, TcScreenshotScanner, scan_tc_screenshots
 
@@ -11,9 +12,11 @@ __all__ = [
     "RehuDocument",
     "RehuFormatError",
     "ScreenshotRename",
+    "TcDescriptionRewriter",
     "TcDocument",
     "TcScreenshotScanner",
     "load_tc",
+    "rewrite_description_images",
     "scan_tc_screenshots",
     "tc_to_rehu_data",
 ]
